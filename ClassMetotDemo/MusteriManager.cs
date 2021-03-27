965672abc712daea3c1226cacb6daec15978bc0c
esra.cetintas@outlook.com.tr <esra.cetintas@outlook.com.tr>
2021-03-27 19:37:51 +0300

    ClassMetotDemo

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        List<Musteri> musteriler = new List<Musteri>();
+        public void MusteriEkle(Musteri musteri)
+        {
+            musteriler.Add(musteri);
+            Console.WriteLine(musteri.Ad + " eklendi...");
+        }
+
+
+        public void MusteriListele()
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad + " " + musteri.Soyad);
+            }
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " silindi");
+            musteriler.Remove(musteri);
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriManager manager = new MusteriManager();
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1234;
+            musteri1.Ad = "Esra";
+            musteri1.Soyad = "Çetintaş";
+            musteri1.Mail = "esracetoo58@gmail.com";
+            manager.MusteriEkle(musteri1);
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 123456;
+            musteri2.Ad = "Samet";
+            musteri2.Soyad = "Çetintaş";
+            musteri2.Mail = "sametcetoo58@gmail.com";
+            manager.MusteriEkle(musteri2);
+
+            Console.WriteLine();
+            manager.MusteriListele();
+
+            Console.WriteLine();
+            manager.MusteriSil(musteri2);
+
+            Console.WriteLine();
+            manager.MusteriListele();
+            Console.ReadKey();
+
+        }
+    }
+}
